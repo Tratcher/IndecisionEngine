4ef6d3764c30a55a131fa1420c1ce75abe0eb98b
Chris R <Tratcher@outlook.com>
2015-10-20 22:05:37 -0700

    Add a simple Story MVC with form input.

---
+++ src/IndecisionEngine/ViewModels/Story/CreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndecisionEngine.ViewModels.Story
+{
+    public class CreateViewModel
+    {
+        [Required]
+        [Display(Name = "Body")]
+        public string Body { get; set; }
+    }
+}
