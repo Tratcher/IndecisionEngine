bd96b8783514c147ed26bcdd64e8015b72cb8ad1
Chris R <tratcher@outlook.com>
2016-02-15 14:57:57 -0800

    Update to RC1

--- src/IndecisionEngine/Controllers/HomeController.cs
+++ src/IndecisionEngine/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace IndecisionEngine.Controllers
 
         public IActionResult Error()
         {
-            return View("~/Views/Shared/Error.cshtml");
+            return View();
         }
     }
 }
