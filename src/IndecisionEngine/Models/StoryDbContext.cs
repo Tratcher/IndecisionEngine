2ca84097e2fe247168f9e56f31b53d70b419e0b1
Chris R <tratcher@outlook.com>
2016-02-17 07:38:26 -0800

    Add missing file

---
+++ src/IndecisionEngine/Models/StoryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace IndecisionEngine.Models
+{
+    public class StoryDbContext : DbContext
+    {
+        public DbSet<StoryEntry> StoryEntries { get; set; }
+    }
+}
