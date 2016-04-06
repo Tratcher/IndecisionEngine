a63d3eef3d72bdf9693a3e42e45c7a36115921ff
Chris R <tratcher@outlook.com>
2016-04-06 07:51:05 -0700

    Getting an e-mail address from twitter.

--- src/IndecisionEngine/Startup.cs
+++ src/IndecisionEngine/Startup.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using IndecisionEngine.Models;
+using IndecisionEngine.Services;
+using Microsoft.AspNet.Authentication.Twitter;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -9,9 +15,8 @@ using Microsoft.Data.Entity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using IndecisionEngine.Models;
-using IndecisionEngine.Services;
-using Microsoft.AspNet.Authorization;
+using Microsoft.Extensions.WebEncoders;
+using Newtonsoft.Json.Linq;
 
 namespace IndecisionEngine
 {
@@ -34,6 +39,9 @@ namespace IndecisionEngine
         }
 
         public IConfigurationRoot Configuration { get; set; }
+
+        // TODO: Temp for Twitter
+        public HttpClient Backchannel { get; set; } = new HttpClient();
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -128,6 +136,71 @@ namespace IndecisionEngine
             {
                 options.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];
                 options.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
+                // TODO: https://github.com/aspnet/Security/issues/765
+                options.Events = new TwitterEvents()
+                {
+                    OnCreatingTicket = async context =>
+                    {
+                        var nonce = Guid.NewGuid().ToString("N");
+
+                        var authorizationParts = new SortedDictionary<string, string>
+                        {
+                            { "oauth_consumer_key", context.Options.ConsumerKey },
+                            { "oauth_nonce", nonce },
+                            { "oauth_signature_method", "HMAC-SHA1" },
+                            { "oauth_timestamp", GenerateTimeStamp() },
+                            { "oauth_token", context.AccessToken },
+                            { "oauth_version", "1.0" }
+                        };
+
+                        var parameterBuilder = new StringBuilder();
+                        foreach (var authorizationKey in authorizationParts)
+                        {
+                            parameterBuilder.AppendFormat("{0}={1}&", UrlEncoder.Default.UrlEncode(authorizationKey.Key), UrlEncoder.Default.UrlEncode(authorizationKey.Value));
+                        }
+                        parameterBuilder.Length--;
+                        var parameterString = parameterBuilder.ToString();
+
+                        var resource_url = "https://api.twitter.com/1.1/account/verify_credentials.json";
+                        var resource_query = "include_email=true";
+                        var canonicalizedRequestBuilder = new StringBuilder();
+                        canonicalizedRequestBuilder.Append(HttpMethod.Get.Method);
+                        canonicalizedRequestBuilder.Append("&");
+                        canonicalizedRequestBuilder.Append(UrlEncoder.Default.UrlEncode(resource_url));
+                        canonicalizedRequestBuilder.Append("&");
+                        canonicalizedRequestBuilder.Append(UrlEncoder.Default.UrlEncode(resource_query));
+                        canonicalizedRequestBuilder.Append("%26");
+                        canonicalizedRequestBuilder.Append(UrlEncoder.Default.UrlEncode(parameterString));
+
+                        var signature = ComputeSignature(context.Options.ConsumerSecret, context.AccessTokenSecret, canonicalizedRequestBuilder.ToString());
+                        authorizationParts.Add("oauth_signature", signature);
+
+                        var authorizationHeaderBuilder = new StringBuilder();
+                        authorizationHeaderBuilder.Append("OAuth ");
+                        foreach (var authorizationPart in authorizationParts)
+                        {
+                            authorizationHeaderBuilder.AppendFormat(
+                                "{0}=\"{1}\", ", authorizationPart.Key, UrlEncoder.Default.UrlEncode(authorizationPart.Value));
+                        }
+                        authorizationHeaderBuilder.Length = authorizationHeaderBuilder.Length - 2;
+
+                        var request = new HttpRequestMessage(HttpMethod.Get, resource_url + "?include_email=true");
+                        request.Headers.Add("Authorization", authorizationHeaderBuilder.ToString());
+
+                        var response = await Backchannel.SendAsync(request, context.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+                        string responseText = await response.Content.ReadAsStringAsync();
+
+                        var result = JObject.Parse(responseText);
+
+                        var email = result.Value<string>("email");
+                        var identity = (ClaimsIdentity)context.Principal.Identity;
+                        if (!string.IsNullOrEmpty(email))
+                        {
+                            identity.AddClaim(new Claim(ClaimTypes.Email, email, ClaimValueTypes.String, "Twitter"));
+                        }
+                    },
+                };
             });
 
             app.UseMvc(routes =>
@@ -136,6 +209,26 @@ namespace IndecisionEngine
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private static string GenerateTimeStamp()
+        {
+            var secondsSinceUnixEpocStart = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return Convert.ToInt64(secondsSinceUnixEpocStart.TotalSeconds).ToString(CultureInfo.InvariantCulture);
+        }
+
+        private string ComputeSignature(string consumerSecret, string tokenSecret, string signatureData)
+        {
+            using (var algorithm = new HMACSHA1())
+            {
+                algorithm.Key = Encoding.ASCII.GetBytes(
+                    string.Format(CultureInfo.InvariantCulture,
+                        "{0}&{1}",
+                        UrlEncoder.Default.UrlEncode(consumerSecret),
+                        string.IsNullOrEmpty(tokenSecret) ? string.Empty : UrlEncoder.Default.UrlEncode(tokenSecret)));
+                var hash = algorithm.ComputeHash(Encoding.ASCII.GetBytes(signatureData));
+                return Convert.ToBase64String(hash);
+            }
         }
 
         // Entry point for the application.
